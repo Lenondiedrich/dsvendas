298a40f8b5e67678dcaf80d8fdb380f523334e17
Lenon Diedrich <lenon.diedrich@gmail.com>
2021-05-08 17:44:51 -0300

    first homolog

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -10,11 +10,11 @@ import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
-	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amout)) "
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
 			+ "FROM Sale AS obj GROUP BY obj.seller")
 	List<SaleSumDTO> amountGroupedBySeller();
 	
-	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals) "
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
 			+ "FROM Sale AS obj GROUP BY obj.seller")
 	List<SaleSuccessDTO> successGroupedBySeller();
 }
